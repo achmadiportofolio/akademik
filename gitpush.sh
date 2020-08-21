da91b8a91be1abfe7dde31b1fd5e21e8ec424484
achmadi <diahmad330@gmail.com>
2020-08-22 06:06:53 +0700

    update

--- gitpush.sh
+++ gitpush.sh
@@ -2,22 +2,22 @@
 
 
    git add .
-   git commit -m "update"
+   git commit -m  $1
    git push 
 
 
 ### Print total arguments and their values
  
-echo "Total Arguments:" $#
-echo "All Arguments values:" $@
+# echo "Total Arguments:" $#
+# echo "All Arguments values:" $@
  
 ### Command arguments can be accessed as
  
-echo "First->"  $1
+#echo "First->"  $1
 echo "Second->" $2
  
 # You can also access all arguments in an array and use them in a script.
  
-args=("$@")
-echo "First->"  ${args[0]} 
-echo "Second->" ${args[1]}
+#args=("$@")
+#echo "First->"  ${args[0]} 
+#echo "Second->" ${args[1]}
